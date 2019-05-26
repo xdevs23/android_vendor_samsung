a1552dfa75530a268985f3e33951492ad0fbbd5a
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-26 22:21:44 +0545

    x

--- a5-common/Android.mk
+++ a5-common/Android.mk
@@ -16,6 +16,17 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter a5ltechn a5ltectc,$(TARGET_DEVICE)),)
+ifneq ($(filter a5ultexx a5ltexx a53gxx,$(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := InputDisabler
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/priv-app/InputDisabler/InputDisabler.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
 
 endif
